e3c4edc4c944c4977d53771223a61b8467afb4c1
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-10-06 19:00:25 -0400

    Did some more work on ancestor.py, still confused, will need some more time with it

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -11,5 +11,47 @@ class Queue():
     def size(self):
         return len(self.queue)
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+    def __str__(self):
+        return f"{self.stack}"
+
+
 def earliest_ancestor(ancestors, starting_node):
     
+    q = Queue()
+    q.enqueue([starting_node])
+    visited = set()
+
+    while q.size() > 0:
+
+        
+
+    
+
+    
+        
+
+'''
+       10
+     /
+    1   2   4  11
+     \ /   / \ /
+      3   5   8
+       \ / \   \
+        6   7   9
+    '''
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(test_ancestors, 6))
+
