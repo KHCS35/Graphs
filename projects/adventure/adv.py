c7d97c978af60922a70f294d60737f677d0e0d9a
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-10-08 15:13:42 -0400

    Starting working on sprint, added a function that takes a room and places it in the map dict in the correct format

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,16 +5,42 @@ from world import World
 import random
 from ast import literal_eval
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,6 +54,31 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+map = {}
+print(f"Current Room Id: {player.current_room.id}")
+print(f"Current Exits: {player.current_room.get_exits()}")
+
+def initialize_room(id, exits):
+    #id is the current room id
+    #exits comes in as a list
+    #create the exits object
+    possible_exits = {}
+    #assign each exit the the default value of '?'
+    for exit in exits:
+        possible_exits[exit] = '?'
+    
+    map[id] = possible_exits
+
+    return map
+
+print(initialize_room(player.current_room.id, player.current_room.get_exits()))
+
+
+def traverse():
+    #choose a direction to check at random
+    rand_direction = random.randint(1, 4)
+
 
 
 
@@ -51,12 +102,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
