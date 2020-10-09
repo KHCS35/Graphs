18c916a63327c17471530bc591930a660cfe4018
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-10-08 22:26:40 -0400

    Did some heavy commenting to get a game plan worked out, and created a initialize_room function

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -55,6 +55,65 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+'''
+What is being asked:
+Construct a traversal graph.
+I'll start in room 0, and it will contain a list of possible exits
+My stating graph should look like this:
+    {
+        0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+    }
+    Those placeholder '?' values will allow me to determine which
+    room have been explored
+If I move south from room 0, my graph should add a new entry
+and some data can be added:
+    {
+        0: {'n': '?', 's': 5, 'w': '?', 'e': '?'},
+        5: {'n': 0, 's': '?', 'w': '?', 'e': '?'}
+    }
+    Since we went south, we found room 5, we need to initialize that room
+    and start putting in data we know of. Such as that south of room 0
+    is room 5, and similarly, north of room 5 is room 0.
+
+My work is complete with my graph has a number of entries equal to 
+the number of rooms. My algorithm should log the path with each time I 
+invoke the player.travel() function.
+
+Each move I make should not be random, it has to be decided by looking at 
+the surrounding rooms and finding out which ones have unexplored areas.
+
+My Game-Plan:
+[X]Create an empty graph
+[X]Create a function that takes the current room id, and it's possible exits
+and adds them to a graph in the right format.
+[]Create a breadth first search that checks the rooms in my graph for
+any unexplored locals.
+'''
+#Creating an empty graph
+map = {}
+
+#Create a function that takes the current room id, and it's possible exits
+#and adds them to a graph in the right format.
+
+print(f"Starting Room: {player.current_room.id}")
+
+def initialize_room(room_id):
+    #room_id will come from player.current_room.id
+    possible_directions = player.current_room.get_exits()
+    exits_obj = {}
+    for direction in possible_directions:
+        #setting each possible direction in the room to the default value
+        exits_obj[direction] = '?'
+    map[room_id] = exits_obj
+
+# initialize_room(0)
+# print(map)
+#Result: {0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}}
+    
+
+
+
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
